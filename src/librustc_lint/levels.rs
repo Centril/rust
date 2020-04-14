56a13c775daab422060f1572159d838331de686f
Mazdak Farrokhzad <twingoow@gmail.com>
2020-04-14 23:33:35 +0200

    lazify 'extract_lint_directives'

--- src/librustc_lint/levels.rs
+++ src/librustc_lint/levels.rs
@@ -2,7 +2,6 @@ use crate::context::{CheckLintNameResult, LintStore};
 use crate::late::unerased_lint_store;
 use rustc_ast::ast;
 use rustc_ast::attr;
-use rustc_ast::unwrap_or;
 use rustc_ast_pretty::pprust;
 use rustc_data_structures::fx::FxHashMap;
 use rustc_errors::{struct_span_err, Applicability, DiagnosticBuilder};
@@ -57,15 +56,18 @@ fn bad_attr(sess: &Session, span: Span) -> DiagnosticBuilder<'_> {
     struct_span_err!(sess, span, E0452, "malformed lint attribute input")
 }
 
-fn extract_lint_reason(sess: &Session, metas: &mut &[ast::NestedMetaItem]) -> Option<Symbol> {
+fn extract_lint_reason(
+    sess: &Session,
+    metas: &[ast::NestedMetaItem],
+    found: &mut bool,
+) -> Option<Symbol> {
     match metas.last()?.meta_item()? {
         // Actual lint names handled later.
         ast::MetaItem { kind: ast::MetaItemKind::Word, .. } => {}
         ast::MetaItem { kind: ast::MetaItemKind::NameValue(name_value), path, span }
             if path == &sym::reason =>
         {
-            // found reason, reslice meta list to exclude it
-            *metas = &metas[0..metas.len() - 1];
+            *found = true;
             // FIXME (#55112): issue unused-attributes lint if we thereby
             // don't have any lint names (`#[level(reason = "foo")]`)
             if let ast::LitKind::Str(rationale, _) = name_value.kind {
@@ -138,45 +140,45 @@ struct LintDirective {
     tool_name: Option<Symbol>,
 }
 
-fn extract_lint_directives(sess: &Session, attrs: &[ast::Attribute]) -> Vec<LintDirective> {
-    let mut directives = Vec::new();
-    for attr in attrs {
-        let level = match Level::from_symbol(attr.name_or_empty()) {
-            None => continue,
-            Some(lvl) => lvl,
-        };
-
-        let meta = unwrap_or!(attr.meta(), continue);
-        attr::mark_used(attr);
-
-        let mut metas = unwrap_or!(meta.meta_item_list(), continue);
-
-        if metas.is_empty() {
-            // FIXME (#55112): issue unused-attributes lint for `#[level()]`
-            continue;
-        }
-
-        // Before processing the lint names, look for a reason (RFC 2383)
-        // at the end.
-        let reason = extract_lint_reason(sess, &mut metas);
-
-        for li in metas {
-            let meta_item = match li.meta_item() {
-                Some(meta_item) if meta_item.is_word() => meta_item,
-                _ => {
-                    error_non_word_lint_attr(sess, li);
-                    continue;
-                }
+fn extract_lint_directives<'a>(
+    sess: &'a Session,
+    attrs: &'a [ast::Attribute],
+) -> impl 'a + Iterator<Item = LintDirective> {
+    attrs
+        .iter()
+        .filter_map(move |attr| {
+            let level = Level::from_symbol(attr.name_or_empty())?;
+            let meta = attr.meta()?;
+            attr::mark_used(attr);
+            let mut list = match meta.kind {
+                // FIXME (#55112): issue unused-attributes lint for `#[level()]`
+                ast::MetaItemKind::List(list) if !list.is_empty() => list,
+                _ => return None,
             };
-            let tool_name = match extract_tool_name(sess, meta_item) {
-                Some(tn) => tn,
-                None => continue,
-            };
-            let name = meta_item.path.segments.last().expect("empty lint name").ident.name;
-            directives.push(LintDirective { level, name, tool_name, reason, span: li.span() });
-        }
-    }
-    directives
+            // Before processing the lint names,
+            // look for a reason (RFC 2383) at the end.
+            let mut found_reason = false;
+            let reason = extract_lint_reason(sess, &list, &mut found_reason);
+            if found_reason {
+                // Found reason, remove it from end of meta list.
+                list.pop();
+            }
+            Some((list, level, reason))
+        })
+        .flat_map(move |(list, level, reason)| {
+            list.into_iter().filter_map(move |li| {
+                let meta_item = match li.meta_item() {
+                    Some(meta_item) if meta_item.is_word() => meta_item,
+                    _ => {
+                        error_non_word_lint_attr(sess, &li);
+                        return None;
+                    }
+                };
+                let tool_name = extract_tool_name(sess, meta_item)?;
+                let name = meta_item.path.segments.last().expect("empty lint name").ident.name;
+                Some(LintDirective { level, name, tool_name, reason, span: li.span() })
+            })
+        })
 }
 
 impl<'s> LintLevelsBuilder<'s> {
